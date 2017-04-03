04ca8b4813d80dc76474ef13a3127d2e8eeb62b9
Marios Fakiolas <m.fakiolas@hotmail.gr>
2017-04-03 16:26:56 +0300

    add Link / fix footer

--- client/scenes/App/components/Footer/index.jsx
+++ client/scenes/App/components/Footer/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Link from 'components/Link';
 import './style.scss';
 
 const Footer = () => (
-  <footer>This is footer</footer>
+  <footer>
+    <p>
+      Made with <span className="heart">&hearts;</span> by <Link
+      href="https://github.com/fakiolinho"
+      target="_blank">
+      Marios
+    </Link>
+    </p>
+  </footer>
 );
 
 export default Footer;
