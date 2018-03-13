468bbe94eff7ee6c3fd5f61e2a64cf28cb665759
Fakiolas Marios <m.fakiolas@hotmail.gr>
2018-03-13 11:07:36 +0200

    fix

--- client/services/repo/actions.js
+++ client/services/repo/actions.js
@@ -3,25 +3,25 @@ import request from 'services/utils/request';
 
 import * as types from './actionTypes';
 
-const getRepo = (owner, repo) => (
-  async dispatch => {
-    try {
-      dispatch({
-        type: types.GET_REPO_REQUEST,
-      });
+const getRepo = (owner, repo) => async dispatch => {
+  try {
+    dispatch({
+      type: types.GET_REPO_REQUEST,
+    });
 
-      const { data } = await request.get(`https://api.github.com/repos/${owner}/${repo}`);
+    const { data } = await request.get(
+      `https://api.github.com/repos/${owner}/${repo}`,
+    );
 
-      dispatch({
-        type: types.GET_REPO_SUCCESS,
-        payload: data,
-      });
-    } catch (err) {
-      dispatch({
-        type: types.GET_REPO_ERROR,
-      });
-    }
+    dispatch({
+      type: types.GET_REPO_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: types.GET_REPO_ERROR,
+    });
   }
-);
+};
 
 export default getRepo;
